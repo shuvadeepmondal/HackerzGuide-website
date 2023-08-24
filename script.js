2edd1f26793a6bc6b943773d5da4e51d58bb029d
SHUVADEEP MNDAL <bongbeast95@gmail.com>
2023-08-24 22:14:25 +0530

    upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+formBtn.addEventListener('click',() =>{
+    loginForm.classList.add('active');
+});
+formClose.addEventListener('click',() =>{
+    loginForm.classList.remove('active');
+});
+window.onscroll=()=>{
+    menu.classList.remove('fa.xmark');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+
+}
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+});
+
+
